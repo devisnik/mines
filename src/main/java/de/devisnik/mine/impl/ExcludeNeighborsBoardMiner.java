fc569adbd129550db6d55b1f40acb69e5718bc5e
Volker Leck <volker.leck@gmail.com>
2013-09-30 14:20:00 +0200

    keep instance of random generator instead of re-creating

--- src/main/java/de/devisnik/mine/impl/ExcludeNeighborsBoardMiner.java
+++ src/main/java/de/devisnik/mine/impl/ExcludeNeighborsBoardMiner.java
@@ -10,8 +10,10 @@ import de.devisnik.mine.Point;
 
 public class ExcludeNeighborsBoardMiner implements IBoardMiner {
 
+	private final Random mRandom = new Random();
+
 	@Override
-	public void mine(IBoard board, int bombs, IField excludeField) {
+	public void mine(final IBoard board, final int bombs, final IField excludeField) {
 		Point dimension = board.getDimension();
 		List<Field> excludeList = createExcludeList((Field) excludeField);
 		if (bombs >= dimension.x * dimension.y - excludeList.size())
@@ -20,7 +22,7 @@ public class ExcludeNeighborsBoardMiner implements IBoardMiner {
 			setBombsRandomly(board, bombs, excludeList, dimension);
 	}
 
-	private List<Field> createExcludeList(Field field) {
+	private List<Field> createExcludeList(final Field field) {
 		Field[] neighbors = field.getNeighbors();
 		ArrayList<Field> excludeList = new ArrayList<Field>();
 		excludeList.add(field);
@@ -29,8 +31,8 @@ public class ExcludeNeighborsBoardMiner implements IBoardMiner {
 		return excludeList;
 	}
 
-	private void setBombsRandomly(IBoard board, int bombs, List<Field> excludeFields,
-			Point dimension) {
+	private void setBombsRandomly(final IBoard board, final int bombs, final List<Field> excludeFields,
+			final Point dimension) {
 		int count = bombs;
 		while (count > 0) {
 			Field field = getRandomField(board, dimension);
@@ -40,15 +42,15 @@ public class ExcludeNeighborsBoardMiner implements IBoardMiner {
 		}
 	}
 
-	private Field getRandomField(IBoard board, Point dimension) {
-		final int pos = new Random().nextInt(dimension.x * dimension.y);
+	private Field getRandomField(final IBoard board, final Point dimension) {
+		final int pos = mRandom.nextInt(dimension.x * dimension.y);
 		final int px = pos % dimension.x;
 		final int py = pos / dimension.x;
 		return (Field) board.getField(px, py);
 	}
 
-	private void setAllOthersBombs(IBoard board, List<Field> excludeFields,
-			Point dimension) {
+	private void setAllOthersBombs(final IBoard board, final List<Field> excludeFields,
+			final Point dimension) {
 		for (int i = 0; i < dimension.y; i++)
 			for (int j = 0; j < dimension.x; j++) {
 				Field field = (Field) board.getField(j, i);
@@ -57,7 +59,7 @@ public class ExcludeNeighborsBoardMiner implements IBoardMiner {
 			}
 	}
 
-	private boolean setBombAt(Field field) {
+	private boolean setBombAt(final Field field) {
 		if (field.isBomb())
 			return false;
 		field.setBomb(true);
