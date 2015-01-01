b2a4d9ea87a08000c67a2ff3ac2205f195d25ea8
Volker Leck <volker.leck@gmail.com>
2015-01-01 22:26:43 +0100

    some renamings in GameCreator

--- android/src/main/java/de/devisnik/android/mine/GameCreator.java
+++ android/src/main/java/de/devisnik/android/mine/GameCreator.java
@@ -5,17 +5,17 @@ import de.devisnik.mine.IGame;
 
 public class GameCreator {
 
-	private final Settings itsSettings;
+	private final Settings settings;
 
-	public GameCreator(Settings configuration) {
-		itsSettings = configuration;
+	public GameCreator(Settings settings) {
+		this.settings = settings;
 	}
 
 	public IGame create() {
-		int[] dimension = itsSettings.getBoardDimension();
+		int[] dimension = settings.getBoardDimension();
 		int width = dimension[0];
 		int height = dimension[1];
-		int bombs = itsSettings.getBombsNumber(width * height);
+		int bombs = settings.getBombsNumber(width * height);
 		return GameFactory.create(width, height, bombs);
 	}
 
