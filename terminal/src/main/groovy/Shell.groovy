e87a45dd3b8e12502e02165bed75fbbd8feaf4e6
Volker Leck <volker.leck@gmail.com>
2017-11-01 23:20:44 +0100

    add Shell class to run groovy shell

---
+++ terminal/src/main/groovy/Shell.groovy
@@ -0,0 +1,25 @@
+import org.codehaus.groovy.tools.shell.IO
+import org.codehaus.groovy.tools.shell.Main
+
+class Shell extends Main {
+
+  private Shell(IO io) {
+    super(io)
+  }
+
+  private start() {
+    String pwd = new File('.').absolutePath
+    println """Welcome to the dark side! (pwd = '$pwd')"""
+
+    def startScript = '''\
+          := interpreterMode
+          := verbosity QUIET '''.stripIndent()
+    startGroovysh(startScript, [])
+  }
+
+  static main(def args) {
+//    setTerminalType('unix' , true /* suppress colors as we get weird char codes in terminal on autocompletion */)
+    setSystemProperty 'groovysh.prompt=is24'
+    new Shell(new IO()).start()
+  }
+}
