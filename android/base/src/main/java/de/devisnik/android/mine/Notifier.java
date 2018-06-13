488376ad509d69c6590f986fb1cb0db36a4e4b4a
Volker Leck <volker.leck@gmail.com>
2018-06-13 12:10:30 +0200

    add notification channel to re-enable notification for Android 8 and above

--- android/base/src/main/java/de/devisnik/android/mine/Notifier.java
+++ android/base/src/main/java/de/devisnik/android/mine/Notifier.java
@@ -1,16 +1,19 @@
 package de.devisnik.android.mine;
 
 import android.app.Notification;
+import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Build;
 
 import de.devisnik.android.mine.base.R;
 import de.devisnik.mine.IGame;
 
 public class Notifier {
     private static final int GAME_INFO_ID = 1;
+    private static final String MINES_CHANNEL = "mines-channel";
     private final Context context;
     private final NotificationManager notificationManager;
     private boolean isDisabled;
@@ -21,6 +24,19 @@ public class Notifier {
         this.gameInfo = gameInfo;
         notificationManager = (NotificationManager) context
                 .getSystemService(Context.NOTIFICATION_SERVICE);
+        createNotificationChannel();
+    }
+
+    private void createNotificationChannel() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            CharSequence name = context.getString(R.string.notification_channel_name);
+            String description = context.getString(R.string.notification_channel_description);
+            int importance = NotificationManager.IMPORTANCE_DEFAULT;
+            NotificationChannel channel = new NotificationChannel(MINES_CHANNEL, name, importance);
+            channel.setDescription(description);
+            channel.setImportance(NotificationManager.IMPORTANCE_LOW);
+            notificationManager.createNotificationChannel(channel);
+        }
     }
 
     public void notifyRunningGame(IGame game) {
@@ -34,15 +50,17 @@ public class Notifier {
         PendingIntent contentIntent = PendingIntent.getActivity(context, 0, notificationIntent,
                 0);
 
-        Notification notification = new Notification.Builder(context)
+        Notification.Builder builder = new Notification.Builder(context)
                 .setContentIntent(contentIntent)
                 .setContentText(contentText)
                 .setContentTitle(contentTitle)
                 .setSmallIcon(R.drawable.bomb_notification)
-                .setAutoCancel(true)
-                .getNotification();
+                .setAutoCancel(true);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            builder.setChannelId(MINES_CHANNEL);
+        }
 
-        notificationManager.notify(GAME_INFO_ID, notification);
+        notificationManager.notify(GAME_INFO_ID, builder.getNotification());
     }
 
     public void clearRunningGame() {
