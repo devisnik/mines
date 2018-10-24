fbbdb0eca4a6276a731f3c7c5ab5a47b179d755d
Volker Leck <volker.leck@gmail.com>
2018-10-24 20:54:55 +0200

    update some build scripts to kotlin

---
+++ robot/build.gradle.kts
@@ -0,0 +1,57 @@
+import org.gradle.jvm.tasks.Jar
+
+plugins {
+    `java-library`
+}
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_1_7
+}
+
+base {
+    archivesBaseName = "de.devisnik.mine.robot"
+}
+version = "1.0.0"
+
+dependencies {
+    api(project(":model"))
+    testImplementation("junit:junit:4.12")
+}
+
+val jarTask = tasks.named<Jar>("jar") {
+    manifest {
+        attributes(
+                mapOf(
+                        "Implementation-Title" to base.archivesBaseName,
+                        "Implementation-Version" to version
+                )
+        )
+    }
+}
+
+val sourcesJarTask = tasks.register<Jar>("sources_jar") {
+    description = "Assembles a jar archive containing the sources."
+    group = "build"
+    classifier = "sources"
+    from(sourceSets["main"].allSource)
+}
+
+val linkJarsTask = tasks.register<Task>("link_jars") {
+    val jarProperties = "${jarTask.get().archivePath}.properties"
+    inputs.files(jarTask, sourcesJarTask)
+    outputs.file(jarProperties)
+    doLast {
+        file(jarProperties).appendText("src=${sourcesJarTask.get().archiveName}\n")
+    }
+}
+
+artifacts {
+    add("archives", sourcesJarTask)
+    add("archives", linkJarsTask.get().outputs.files.single()) {
+        type = "jar.properties" // ensure correct file extension
+    }
+}
+
+tasks.named<Task>("assemble") {
+    dependsOn(linkJarsTask)
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,9 @@
+include(":model")
+include(":android:base")
+include(":android:installed")
+include(":android:instant")
+include(":robot")
+include(":swt")
+include(":gwt")
+include(":terminal")
+
