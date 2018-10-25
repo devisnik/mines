c5c910238274a1b0b74c442458a1d17310e4fdc1
Volker Leck <volker.leck@gmail.com>
2018-10-25 15:35:04 +0200

    convert android root build script to kotlin

---
+++ android/build.gradle.kts
@@ -0,0 +1,12 @@
+mapOf(
+        "VERSION_CODE" to 63,
+        "VERSION_NAME" to "1.11.2",
+        "APP_NAME" to "de.devisnik.android.mine",
+        "MIN_SDK" to 21,
+        "TARGET_SDK" to 27,
+        "COMPILE_SDK" to 28,
+        "BUILD_TOOLS" to "28.0.3"
+)
+        .forEach { e ->
+            project.extra.set(e.key, e.value)
+        }
