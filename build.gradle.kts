f01d9b874c77ce80dbc20ec61ea9827b33b93cf3
Volker Leck <volker.leck@gmail.com>
2018-10-25 15:15:34 +0200

    convert root build script to kotlin

---
+++ build.gradle.kts
@@ -0,0 +1,22 @@
+buildscript {
+    repositories {
+        google()
+        jcenter()
+    }
+    dependencies {
+        classpath("com.android.tools.build:gradle:3.2.1")
+        classpath("com.novoda:gradle-android-command-plugin:2.0.1")
+        classpath("com.github.ben-manes:gradle-versions-plugin:0.20.0")
+        classpath("com.github.triplet.gradle:play-publisher:1.2.2")
+        classpath("com.getkeepsafe.dexcount:dexcount-gradle-plugin:0.8.4")
+    }
+}
+
+allprojects {
+    repositories {
+        google()
+        jcenter()
+    }
+    apply(plugin = "com.github.ben-manes.versions")
+    apply(plugin = "idea")
+}
