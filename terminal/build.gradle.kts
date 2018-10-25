a64e6a6eba236ede8f1434b38964d854771f698d
Volker Leck <volker.leck@gmail.com>
2018-10-25 14:41:05 +0200

    migrate terminal build script to kotlin

---
+++ terminal/build.gradle.kts
@@ -0,0 +1,16 @@
+plugins {
+    groovy
+    application
+}
+
+application {
+    mainClassName = "Shell"
+}
+
+dependencies {
+    implementation(project(":model"))
+    implementation(project(":robot"))
+    implementation(localGroovy())
+    implementation("commons-cli:commons-cli:1.3.1")
+    implementation("jline:jline:2.14.2")
+}
