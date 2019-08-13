8bcd7cb07cd37c8f05113105f136ba118a3440ab
Volker Leck <volker.leck@gmail.com>
2019-08-13 16:03:11 +0200

    Update to Gradle 5.5.1 and AGP 3.4.2

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,12 +4,23 @@ buildscript {
         jcenter()
     }
     dependencies {
-        classpath("com.android.tools.build:gradle:3.3.1")
+        classpath("com.android.tools.build:gradle:3.4.2")
         classpath("com.novoda:gradle-android-command-plugin:2.0.1")
         classpath("com.github.ben-manes:gradle-versions-plugin:0.20.0")
         classpath("com.github.triplet.gradle:play-publisher:1.2.2")
         classpath("com.getkeepsafe.dexcount:dexcount-gradle-plugin:0.8.4")
     }
+}
+
+plugins {
+  id("com.gradle.build-scan") version "2.4"
+}
+
+buildScan {
+    setTermsOfServiceUrl("https://gradle.com/terms-of-service")
+    setTermsOfServiceAgree("yes")
+    publishAlways()
+    isCaptureTaskInputFiles = true
 }
 
 allprojects {
