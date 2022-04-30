fef1fb49ee9e0fa92def33789d896eb789625c01
Volker Leck <volker.leck@gmail.com>
2022-04-30 19:03:05 +0200

    Minor cleanup

--- android/base/src/main/java/de/devisnik/android/mine/device/BaseDevice.java
+++ android/base/src/main/java/de/devisnik/android/mine/device/BaseDevice.java
@@ -4,15 +4,8 @@ import android.os.Build;
 
 public abstract class BaseDevice implements IDevice {
 
-	private final int mBuildVersion;
-
-	public BaseDevice() {
-		mBuildVersion = Integer.parseInt(Build.VERSION.SDK);
-	}
-
 	@Override
 	public int getBuildVersion() {
-		return mBuildVersion;
+		return Build.VERSION.SDK_INT;
 	}
-
 }
