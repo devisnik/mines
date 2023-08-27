a0a2faa6c8ea370f2bd8107516152f67f35f0f3a
Volker Leck <volker.leck@gmail.com>
2023-08-27 13:07:52 +0200

    Target android 33

--- buildSrc/src/main/kotlin/Android.kt
+++ buildSrc/src/main/kotlin/Android.kt
@@ -1,11 +1,11 @@
 object Android {
-    const val VERSION_CODE = 67
-    const val VERSION_NAME = "1.13.2"
+    const val VERSION_CODE = 68
+    const val VERSION_NAME = "1.14.0"
     const val APP_NAME = "de.devisnik.android.mine"
 
     const val MIN_SDK = 23
-    const val TARGET_SDK = 31
+    const val TARGET_SDK = 33
     const val COMPILE_SDK = 33
 
-    const val BUILD_TOOLS = "33.0.1"
+    const val BUILD_TOOLS = "33.0.2"
 }
