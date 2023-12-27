37e7fd3518a76341b9e0da9e9d84c70d4be07b81
Volker Leck <volker.leck@gmail.com>
2023-12-27 18:59:28 +0100

    Target Android 14

--- buildSrc/src/main/kotlin/Android.kt
+++ buildSrc/src/main/kotlin/Android.kt
@@ -1,11 +1,11 @@
 object Android {
-    const val VERSION_CODE = 69
-    const val VERSION_NAME = "1.14.0"
+    const val VERSION_CODE = 70
+    const val VERSION_NAME = "1.15.0"
     const val APP_NAME = "de.devisnik.android.mine"
 
     const val MIN_SDK = 23
-    const val TARGET_SDK = 33
-    const val COMPILE_SDK = 33
+    const val TARGET_SDK = 34
+    const val COMPILE_SDK = 34
 
-    const val BUILD_TOOLS = "33.0.2"
+    const val BUILD_TOOLS = "34.0.0"
 }
